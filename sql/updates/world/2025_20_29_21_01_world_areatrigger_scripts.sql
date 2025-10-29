0a35d5709b731a50cbb0894b3155d11d84cacb24
Kittnz <315169700@qq.com>
2025-10-29 21:09:02 +0800

    [DB] Add the script name that is missing from the database.

---
+++ sql/updates/world/2025_20_29_21_01_world_areatrigger_scripts.sql
@@ -0,0 +1,111 @@
+/* Queries to be executed on harbringers_w - 127.0.0.1:3307 */
+SET FOREIGN_KEY_CHECKS = 0;
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '165065',
+    'auchindoun_kaathar_at_fissure'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '157787',
+    'auchindoun_nyami_at_radiant_star'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '157001',
+    'auchindoun_teronogor_areatrigger_chaos_wave'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '153725',
+    'auchindoun_azzakel_at_fel_spark'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '9509',
+    'at_timeless_isle_ordos'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '8317',
+    'at_flameweaver_koegler'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('7548', 'at_lilian_voss');
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('7527', 'at_rattlegore');
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('6854', 'at_helms_bed_lake');
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('6224', 'areatrigger_th_si7');
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '6219',
+    'areatrigger_th_crime_scene'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '6218',
+    'areatrigger_th_wollerton_stead'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '6214',
+    'areatrigger_th_stormwind_graves'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('6135', 'SmartTrigger');
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('6033', 'SmartTrigger');
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '157793',
+    'auchindoun_at_arcane_bomb'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  (
+    '5483',
+    'at_follow_the_tracks_5483'
+  );
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('5482', 'at_caravan_scene_5482');
+
+insert into `harbringers_w`.`areatrigger_scripts` (`entry`, `ScriptName`)
+values
+  ('5465', 'at_groldoms_farm_5465');
